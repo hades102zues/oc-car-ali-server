74717954206c22497d41204f2e21f3bf8bd97b0c
Joshua Wiggins <joshua_kar@hotmail.com>
2019-03-20 12:06:49 -0400

    oof

--- knexfile.js
+++ knexfile.js
@@ -18,8 +18,7 @@ module.exports = {
 	},
 	production: {
 	client: "pg",
-	connection: process.env.DATABASE_URL || 'postgres://husylkraaixkpb:4429eb916824071e496cdd89e4c27dadea82f98c117e0a8866d20eafd486a2ee@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d6v2ejec63ddjo',
-	ssl: true,
+	connection: process.env.DATABASE_URL,
 	migrations: {
 		directory: path.join(__dirname, "db", "migrations")
 	},
